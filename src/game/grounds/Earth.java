440738d0876613f1584ec63a58a56687aa3300e6
XCZ1007 <sengcheng.shee@icloud.com>
2025-09-19 23:41:11 +0800

    Update Earth class
    -Introduce TeleportationCircle to both map.

--- src/game/grounds/Earth.java
+++ src/game/grounds/Earth.java
@@ -6,6 +6,7 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
 import game.actors.Player;
 import game.grounds.teleportable.TeleDoor;
+import game.grounds.teleportable.TeleportationCircle;
 
 import java.util.Arrays;
 import java.util.List;
@@ -51,10 +52,18 @@ public class Earth extends World {
         GameMap gameMap1 = new GameMap("Minecraft", groundCreator, map2);
         this.addGameMap(gameMap1);
 
-        TeleDoor teleDoor1 = new TeleDoor(List.of(gameMap.at(10,4), gameMap1.at(4,4)));
+        TeleDoor teleDoor1 = new TeleDoor(List.of(gameMap.at(10,4),
+                gameMap1.at(4,4)));
         gameMap.at(4,4).setGround(teleDoor1);
 
         gameMap1.at(10,4).setGround(teleDoor1);
+
+        TeleportationCircle teleCircle1 = new TeleportationCircle(List.of(
+                gameMap.at(12,7),gameMap1.at(6,7)));
+
+        gameMap.at(6,7).setGround(teleCircle1);
+
+        gameMap1.at(12,7).setGround(teleCircle1);
 
         Player player = new Player("Explorer", 'ඞ', 100);
         this.addPlayer(player, gameMap.at(1, 1));
