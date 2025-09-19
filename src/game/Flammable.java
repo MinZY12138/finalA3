e7417b1294a348825ec3984a7addb48dce0fc79b
XCZ1007 <sengcheng.shee@icloud.com>
2025-09-19 22:26:28 +0800

    Created a Flammable interface
    -Use to represent any flammable object in this system (e.g., Player (is an Actor))
    
    -Behaviour of burn is introduced
    
    -With proper documentation

---
+++ src/game/Flammable.java
@@ -0,0 +1,22 @@
+package game;
+
+/**
+ * <h1>Interface Flammable</h1>
+ *
+ * <p>
+ *     Representing flammable object in the system.
+ *     Class that implement this should define how they
+ *     react with when the object is being burned.
+ * </p>
+ *
+ * @author Shee Seng Cheng
+ * @version 1.0
+ */
+public interface Flammable
+{
+    /**
+     * Define a behaviour when the object is being burned.
+     * @param damage the amount of damage cause by this burn.
+     */
+    void burn(int damage);
+}
