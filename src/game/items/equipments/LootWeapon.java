801d7f35835f1305c81a4b7383c7f3b8a001132f
cheehsian-1120 <ctay0040@student.monash.edu>
2025-09-24 18:44:36 +0800

    Complete LootWeapon abstract class - Make all attribute final - Define hit abstract method - Implement allowableActions method to allow every weapon return AttackAction

--- src/game/items/equipments/LootWeapon.java
+++ src/game/items/equipments/LootWeapon.java
@@ -1,35 +1,50 @@
 package game.items.equipments;
 
+import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
 import edu.monash.fit2099.engine.weapons.Weapon;
+import game.actions.AttackAction;
 
 import java.util.Random;
 
 public abstract class LootWeapon extends Item implements Weapon {
 
-    protected int damage;
+    protected final int DAMAGE;
 
-    protected int hitRate;
+    protected final int HIT_RATE;
 
-    protected String verb;
+    protected final String VERB;
+
+    public static final Random RAND = new Random();
 
     public LootWeapon(String name, char displayChar, boolean portable, int damage, int hitRate,
                       String verb) {
         super(name, displayChar, portable);
-        this.damage = damage;
-        this.hitRate = hitRate;
-        this.verb = verb;
+        this.DAMAGE = damage;
+        this.HIT_RATE = hitRate;
+        this.VERB = verb;
+    }
+
+    public void hit(Actor attacker, Actor target, GameMap map) {}
+
+    @Override
+    public final String attack(Actor attacker, Actor target, GameMap map) {
+        if (!(RAND.nextInt(100) <= this.HIT_RATE)) {
+            return attacker + " misses " + target + ".";
+        }
+
+        target.hurt(this.DAMAGE);
+        this.hit(attacker, target, map);
+        return String.format("%s %s %s for %d damage", attacker, this.VERB, target, this.DAMAGE);
     }
 
     @Override
-    public String attack(Actor attacker, Actor target, GameMap map) {
-        Random rand = new Random();
-        if (!(rand.nextInt(100) <= this.hitRate)) {
-            return attacker + " misses " + target + ".";
-        }
-        target.hurt(damage);
-        return String.format("%s %s %s for %d damage", attacker, verb, target, damage);
+    public ActionList allowableActions(Actor otherActor, Location location) {
+        ActionList actions = super.allowableActions(otherActor, location);
+        actions.add(new AttackAction(otherActor, location.toString(), this.VERB, this));
+        return actions;
     }
 }
