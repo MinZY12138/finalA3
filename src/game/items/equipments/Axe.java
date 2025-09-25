563e0ae06703a9148ce577ffd375af5f7521e278
cheehsian-1120 <ctay0040@student.monash.edu>
2025-09-25 19:16:31 +0800

    Complete LootWeapon subclasses - Complete the Axe class by adding the chance of hit. - Complete the Torch class by adding the chance of hit and the feature of burning the target actor. - Complete the Bow class with adding the range feature. - Define constants in every subclass to avoid using magic numbers and make it meaningful.

--- src/game/items/equipments/Axe.java
+++ src/game/items/equipments/Axe.java
@@ -1,9 +1,29 @@
 package game.items.equipments;
 
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actors.statuses.Bleeding;
+
 public class Axe extends LootWeapon {
 
-    public Axe(String name, char displayChar, boolean portable, int damage, int hitRate,
-               String verb) {
-        super("axe", 'p', true, 15, 75, "hacks");
+    private static final int HIT_DMG = 15;
+
+    private static final int HIT_RATE = 75;
+
+    private static final int BLEED_DMG = 10;
+
+    private static final int DURATION = 2;
+
+    public Axe() {
+        super("axe", 'p', true, HIT_DMG, HIT_RATE, "hacks");
+    }
+
+    @Override
+    public void hit(Actor attacker, Actor target, GameMap map) {
+        int chance = 50;
+
+        if (RAND.nextInt(100) <= chance) {
+            target.addStatus(new Bleeding(target, BLEED_DMG, DURATION));
+        }
     }
 }
