61413d945ad93ca49df57e1b994b5c09c3230bb7
Min Zhengyuan <zmin0008@student.monash.edu>
2025-10-03 08:43:15 +0800

    feat(req4): add Coating interface for weapon coating system
    - Introduced Coating interface in game.items.equipments
    - Defines name() and applyOnHit(...) for coating strategy
    - Forms base of REQ4 (coating) implementation

--- src/game/items/equipments/coat/Coating.java
+++ src/game/items/equipments/coat/Coating.java
@@ -1,4 +1,9 @@
 package game.items.equipments.coat;
 
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
 public interface Coating {
+    String name();
+    void applyOnHit(Actor attacker, Actor target, GameMap map);
 }
