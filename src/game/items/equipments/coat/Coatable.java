8105e071dbbbe905237784692eb54cd3c77e4306
Min Zhengyuan <zmin0008@student.monash.edu>
2025-10-03 08:46:14 +0800

    feat(req4): add Coatable interface for weapons
    - Introduced Coatable interface in game.items.equipments
    - Supports get/set/clear coating and coatedName()
    - Complements Coating interface added earlier

--- src/game/items/equipments/coat/Coatable.java
+++ src/game/items/equipments/coat/Coatable.java
@@ -1,4 +1,15 @@
 package game.items.equipments.coat;
 
+import java.util.Optional;
+
 public interface Coatable {
+
+
+    Optional<Coating> getCoating();
+
+    void setCoating(Coating coating);
+
+    void clearCoating();
+
+    String coatedName();
 }
