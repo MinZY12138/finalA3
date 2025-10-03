fca3f68ee5dab6fcea2cc871e11448c0388833ae
Min Zhengyuan <zmin0008@student.monash.edu>
2025-10-03 09:45:19 +0800

    feat(req4): add YewberryCoating and Poisoning effect
    - Implemented Poisoning status (4 dmg/turn, 5 turns) extending ContinuousDamage
    - Implemented YewberryCoating that applies Poisoning on weapon hit

--- src/game/actors/statuses/Poisoning.java
+++ src/game/actors/statuses/Poisoning.java
@@ -1,4 +1,31 @@
 package game.actors.statuses;
 
-public class Poisoning {
+import edu.monash.fit2099.engine.actors.Actor;
+
+/**
+ * <h1>Poisoning class</h1>
+ * <p>
+ * Represent a status of {@code Poisoning} on the {@link Actor}, inheriting from
+ * the {@link ContinuousDamage} abstract class.
+ * </p>
+ *
+ * <p>Effect: deals 4 damage per turn for a fixed number of turns.</p>
+ *
+ * @author Min Zhengyuan
+ * @version 1.0.0
+ * @since 2025-10-03
+ */
+public class Poisoning extends ContinuousDamage {
+
+    private static final int DAMAGE_PER_TURN = 4;
+
+    /**
+     * The constructor of the Poisoning class.
+     *
+     * @param actor    the actor who is poisoned
+     * @param duration total poisoning turns (e.g., 5)
+     */
+    public Poisoning(Actor actor, int duration) {
+        super(actor, DAMAGE_PER_TURN, duration, "is poisoned");
+    }
 }
--- src/game/items/equipments/coat/YewberryCoating.java
+++ src/game/items/equipments/coat/YewberryCoating.java
@@ -1,4 +1,22 @@
 package game.items.equipments.coat;
 
-public class YewberryCoating {
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actors.statuses.Poisoning;
+
+/**
+ * Coating that poisons the target for 5 turns (-4 HP per turn).
+ */
+public class YewberryCoating implements Coating {
+
+    @Override
+    public String name() {
+        return "Yewberry";
+    }
+
+    @Override
+    public void applyOnHit(Actor attacker, Actor target, GameMap map) {
+        // Attach Poisoning status to target
+        target.addStatus(new Poisoning(target, 5));
+    }
 }
