195e40b569486a6180c8730fecd2fcb27f0ec987
34471839 <jngg0160@student.monash.edu>
2025-10-07 20:14:40 +0800

    cancel the isinstance in consumeBehaviour by adding an enum class and set the abilities for the fruit

---
+++ src/game/items/fruits/Food.java
@@ -0,0 +1,14 @@
+package game.items.fruits;
+
+/**
+ * <h1>Food enum</h1>
+ *
+ * Represent the action of food can have
+ *
+ * @author Ng Jun Jie
+ * @version 1.0
+ */
+public enum Food
+{
+    CONSUME
+}
