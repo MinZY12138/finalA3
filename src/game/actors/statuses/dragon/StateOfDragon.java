a6eb3d6f177711d76ef83bbca6b4daf9dc380a3d
XCZ1007 <sengcheng.shee@icloud.com>
2025-10-08 15:59:17 +0800

    Update all classes with proper javac documentation.

--- src/game/actors/statuses/dragon/StateOfDragon.java
+++ src/game/actors/statuses/dragon/StateOfDragon.java
@@ -1,11 +1,32 @@
 package game.actors.statuses.dragon;
 
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actors.animals.Animal;
+
 import java.util.Random;
 
+/**
+ * <h1>
+ *     Abstract class StateOfDragon
+ * </h1>
+ *
+ * <p>
+ *     Represent the parent's state of the dragon.
+ *     It has all the default method to manage the transformation to
+ *     different state of the dragon.
+ * </p>
+ */
 public abstract class StateOfDragon implements DragonState
 {
+    /**
+     * Use to get random number.
+     */
     private static final Random RAND = new Random();
 
+    /**
+     * Method to decide which state next.
+     * @return {@link DragonState} the chosen state of the dragon.
+     */
     @Override
     public DragonState getNextState(){
         int chance  = RAND.nextInt(StateDragonInfo.RANDOM_BOUND.getINFO());
@@ -31,13 +52,34 @@ public abstract class StateOfDragon implements DragonState
         }
     }
 
+    /**
+     * A simple name representation of the current state.
+     * @return {@code String} the name of the state.
+     */
     @Override
     public String getStateName(){
         return this.getClass().getSimpleName();
     }
 
+    /**
+     * Getter to get the state duration.
+     * @return {@code int} duration of the state.
+     */
     @Override
-    public int setStateDuration() {
+    public int getStateDuration() {
         return StateDragonInfo.DURATION.getINFO();
+    }
+
+    /**
+     * Set the dragon effect when is in this State.
+     *
+     * @param dragon the dragon
+     * @param map    the map where the dragon was
+     */
+    @Override
+    public void setDragonAction(Animal dragon, GameMap map)
+    {
+        dragon.resistanceToWarm = false;
+        dragon.resetBehaviourMap();
     }
 }
