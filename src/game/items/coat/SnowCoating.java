936613d1cbe9485fe9811f074d58e88fc11b4308
cheehsian-1120 <ctay0040@student.monash.edu>
2025-10-08 13:26:04 +0800

    Refactor the class that related to the coating system - Remove unnecessary classes from the Coatable interface. - Change Coating class from interface to abstract class - Refactor SnowCoating class to reuse the code of engine - Define constant in YewBerryCoating class to avoid magic numbers.

--- src/game/items/coat/SnowCoating.java
+++ src/game/items/coat/SnowCoating.java
@@ -3,7 +3,7 @@ package game.items.coat;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.actors.animals.Warmable;
-import game.actors.statuses.Frosting;
+import game.actors.statuses.Frostbiting;
 import game.actors.abilities.Abilities;
 
 /**
@@ -14,7 +14,7 @@ import game.actors.abilities.Abilities;
  * When the coated weapon hits a target:
  * <ul>
  *     <li>If the target has the {@code COLD_RESISTANT} ability, the effect is ignored.</li>
- *     <li>Otherwise, if the target exposes {@link Warmable} capability, a {@link Frosting}
+ *     <li>Otherwise, if the target exposes {@link Warmable} capability, a {@link Frostbiting}
  *     status is applied for 3 turns.</li>
  * </ul>
  * </p>
@@ -26,23 +26,19 @@ import game.actors.abilities.Abilities;
  * @author Min Zhengyuan
  * @version 1.0
  */
-public class SnowCoating implements Coating {
+public class SnowCoating extends Coating {
 
-    /**
-     * Get the name of this coating.
-     *
-     * @return The string {@code "Snow"}.
-     */
-    @Override
-    public String name() {
-        return "Snow";
+    private static final int DURATION = 3;
+
+    public SnowCoating() {
+        super("Snow");
     }
 
     /**
      * Apply the frostbite effect when the coated weapon hits a target.
      * <p>
      * - Ignores targets with {@code COLD_RESISTANT} ability.<br>
-     * - Applies {@link Frosting} to any target exposing {@link Warmable}.
+     * - Applies {@link Frostbiting} to any target exposing {@link Warmable}.
      * </p>
      *
      * @param attacker The actor performing the attack.
@@ -52,13 +48,9 @@ public class SnowCoating implements Coating {
     @Override
     public void applyOnHit(Actor attacker, Actor target, GameMap map) {
         // Skip if target is cold-resistant (e.g., spawned from Tundra)
-        if (target.hasAbility(Abilities.COLD_RESISTANT)) {
-            return;
+        if (!target.hasAbility(Abilities.COLD_RESISTANT)) {
+            //Game rule actor in this system should be warmable accept they have COLD_RESISTANT.
+            target.addStatus(new Frostbiting((Warmable) target, DURATION));
         }
-
-        // Apply frostbite if target exposes Warmable capability (no instanceof used)
-        target.asCapability(Warmable.class).ifPresent(warmable -> {
-            target.addStatus(new Frosting(warmable, 3));
-        });
     }
 }
