5f99f55d8cda7b57149f2f410eb282b623ad7c21
XCZ1007 <sengcheng.shee@icloud.com>
2025-10-29 03:51:41 +0800

    Earth class -change the gameMap to more meaningful variable for better readability and clarity

--- src/game/grounds/Earth.java
+++ src/game/grounds/Earth.java
@@ -61,44 +61,44 @@ public class Earth extends World {
         );
 
 
-        GameMap gameMap1 = new GameMap("Forest", groundCreator, map);
-        this.addGameMap(gameMap1);
+        GameMap forest = new GameMap("Forest", groundCreator, map);
+        this.addGameMap(forest);
 
         Player player = new Player("Explorer", 'ඞ', 100, 30);
-        this.addPlayer(player, gameMap1.at(1, 1));
+        this.addPlayer(player, forest.at(1, 1));
 
         // req 1
-        GameMap gameMap2 = new GameMap("Plains", groundCreator, map2);
-        this.addGameMap(gameMap2);
+        GameMap plains = new GameMap("Plains", groundCreator, map2);
+        this.addGameMap(plains);
 
-        Location locationDoor1 = gameMap1.at(4,4);
+        Location locationDoor1 = forest.at(4,4);
         TeleDoor teleDoor1 = new TeleDoor(
                 List.of(
-                        gameMap1.at(10, 4), gameMap2.at(4, 4)
+                        forest.at(10, 4), plains.at(4, 4)
                 ), locationDoor1
         );
         locationDoor1.setGround(teleDoor1);
 
-        Location locationDoor2 = gameMap2.at(4,4);
+        Location locationDoor2 = plains.at(4,4);
         TeleDoor teleDoor2 = new TeleDoor(
                 List.of(
-                        gameMap1.at(4, 4), gameMap2.at(10, 4)
+                        forest.at(4, 4), plains.at(10, 4)
                 ), locationDoor2
         );
         locationDoor2.setGround(teleDoor2);
 
-        Location locationCricle1 = gameMap1.at(12, 7);
+        Location locationCricle1 = forest.at(12, 7);
         TeleportationCircle teleCircle1 = new TeleportationCircle(
                 List.of(
-                        gameMap1.at(18, 7), gameMap2.at(12, 7)
+                        forest.at(18, 7), plains.at(12, 7)
                 ), locationCricle1
         );
         locationCricle1.setGround(teleCircle1);
 
-        Location locationCircle2 = gameMap2.at(12,7);
+        Location locationCircle2 = plains.at(12,7);
         TeleportationCircle teleCircle2 = new TeleportationCircle(
                 List.of(
-                        gameMap2.at(18,7), gameMap1.at(12,7)
+                        plains.at(18,7), forest.at(12,7)
                 ),
                 locationCircle2
         );
@@ -107,7 +107,7 @@ public class Earth extends World {
 
         TeleportCube cube1 = new TeleportCube(
                 List.of(
-                        gameMap1.at(1, 1), gameMap2.at(1, 1)
+                        forest.at(1, 1), plains.at(1, 1)
                 )
         );
 
@@ -118,25 +118,25 @@ public class Earth extends World {
         Spawnable deer = Deer::new;
         Spawnable wolf = Wolf::new;
 
-        gameMap1.at(8, 9).setGround(new Tundra(List.of(bear)));
-        gameMap2.at(0, 0).setGround(new Tundra(List.of(wolf)));
-        gameMap1.at(0, 5).setGround(new Cave(List.of(bear, wolf, deer)));
-        gameMap2.at(0, 5).setGround(new Cave(List.of(bear, wolf)));
-        gameMap1.at(0, 9).setGround(new Meadow(List.of(deer)));
-        gameMap2.at(0, 9).setGround(new Meadow(List.of(deer, wolf)));
+        forest.at(8, 9).setGround(new Tundra(List.of(bear)));
+        plains.at(0, 0).setGround(new Tundra(List.of(wolf)));
+        forest.at(0, 5).setGround(new Cave(List.of(bear, wolf, deer)));
+        plains.at(0, 5).setGround(new Cave(List.of(bear, wolf)));
+        forest.at(0, 9).setGround(new Meadow(List.of(deer)));
+        plains.at(0, 9).setGround(new Meadow(List.of(deer, wolf)));
 
 
-        gameMap2.at(2, 2).addItem(new Apple());
-        gameMap1.at(2, 2).addItem(new Apple());
-        gameMap2.at(6, 7).addItem(new Hazelnut());
-        gameMap1.at(6, 7).addItem(new Hazelnut());
-        gameMap2.at(9, 9).addItem(new YewBerry());
-        gameMap1.at(9, 9).addItem(new YewBerry());
+        plains.at(2, 2).addItem(new Apple());
+        forest.at(2, 2).addItem(new Apple());
+        plains.at(6, 7).addItem(new Hazelnut());
+        forest.at(6, 7).addItem(new Hazelnut());
+        plains.at(9, 9).addItem(new YewBerry());
+        forest.at(9, 9).addItem(new YewBerry());
 
         // req 3
-        Location location1 = RandomLocation.randomChooseLocation(gameMap1);
-        Location location2 = RandomLocation.randomChooseLocation(gameMap1);
-        Location location3 = RandomLocation.randomChooseLocation(gameMap2);
+        Location location1 = RandomLocation.randomChooseLocation(forest);
+        Location location2 = RandomLocation.randomChooseLocation(forest);
+        Location location3 = RandomLocation.randomChooseLocation(plains);
 
         Axe axe = new Axe(WeaponType.AXE, StatusType.BLEEDING);
         location1.addItem(axe);
@@ -159,14 +159,14 @@ public class Earth extends World {
         Growthable appleSproutsF = new AppleSprouts(false);
         appleSproutsF.setNextStage(appleSaplingF);
 
-        gameMap1.at(0,0).setGround(appleSproutsF);
+        forest.at(0,0).setGround(appleSproutsF);
 
         // Plains
         AppleTree matureAppleTreeP = new AppleTree();
 
         Growthable appleSproutsP = new AppleSprouts(true);
         appleSproutsP.setNextStage(matureAppleTreeP);
-        gameMap2.at(0,0).setGround(appleSproutsP);
+        plains.at(0,0).setGround(appleSproutsP);
         //----YewBerry----
         //Forest
         YewBerryTree matureYewBerryF = new YewBerryTree();
@@ -174,7 +174,7 @@ public class Earth extends World {
         Growthable yewBerrySaplingF = new YewBerrySapling(false);
         yewBerrySaplingF.setNextStage(matureYewBerryF);
 
-        gameMap1.at(5,0).setGround(yewBerrySaplingF);
+        forest.at(5,0).setGround(yewBerrySaplingF);
 
         //Plains
         YewBerryTree matureYewBerryP = new YewBerryTree();
@@ -182,7 +182,7 @@ public class Earth extends World {
         Growthable yewBerrySaplingP = new YewBerrySapling(true);
         yewBerrySaplingP.setNextStage(matureYewBerryP);
 
-        gameMap2.at(5,0).setGround(yewBerrySaplingP);
+        plains.at(5,0).setGround(yewBerrySaplingP);
 
     }
 }
