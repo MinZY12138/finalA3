c0988d9bccb829c574f77c7fc31a27d9593a4e03
Min Zhengyuan <zmin0008@student.monash.edu>
2025-11-01 07:51:11 +0800

    Completed DimensionalStoreType which designed three stores.

--- src/main/java/game/grounds/dimensional/DimensionalStoreType.java
+++ src/main/java/game/grounds/dimensional/DimensionalStoreType.java
@@ -1,4 +1,25 @@
 package game.grounds.dimensional;
 
+/**
+ * Represents the different styles of Mysterio stores that can manifest from a dimensional rift.
+ */
 public enum DimensionalStoreType {
+    ARMOURY("Armoury"),
+    ALCHEMY("Alchemy"),
+    CURIOSITY("Curiosity");
+
+    private final String description;
+
+    DimensionalStoreType(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Readable description for menu and logging.
+     *
+     * @return textual description
+     */
+    public String getDescription() {
+        return description;
+    }
 }
