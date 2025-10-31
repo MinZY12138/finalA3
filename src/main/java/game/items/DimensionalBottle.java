c94e7520e46a2e051d2a5b8f3d0b61a5b2e73205
Min Zhengyuan <zmin0008@student.monash.edu>
2025-11-01 06:58:15 +0800

    Created DimensionalBottle

--- src/main/java/game/items/DimensionalBottle.java
+++ src/main/java/game/items/DimensionalBottle.java
@@ -1,4 +1,34 @@
 package game.items;
 
-public class DimensionalBottle {
+import java.util.List;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.Location;
+import game.items.ItemInfo;
+
+/**
+ * Dimensional Bottle (minimal, TeleportCube-style)
+ * Smash on current tile only.
+ */
+public final class DimensionalBottle extends Item {
+
+    /**
+     * Constructor for DimensionalBottle.
+     */
+    public DimensionalBottle() {
+        super(ItemInfo.DIMENSIONAL_BOTTLE.getNAME(),
+                ItemInfo.DIMENSIONAL_BOTTLE.getCHAR(),
+                ItemInfo.DIMENSIONAL_BOTTLE.isPORTABLE());
+    }
+
+    /**
+     * Currently no actions are available.
+     * You can later add SmashHereAction to make it functional.
+     */
+    @Override
+    public List<Action> allowableActions(Actor owner, Location here, String direction) {
+        return List.of(); // No actions yet
+    }
 }
