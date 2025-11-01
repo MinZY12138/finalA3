d3684ccfc0e5971f95817a9d508135764d18b973
Min Zhengyuan <zmin0008@student.monash.edu>
2025-11-01 09:24:55 +0800

    Create store map in earth.

--- src/main/java/game/grounds/Earth.java
+++ src/main/java/game/grounds/Earth.java
@@ -30,6 +30,7 @@ public class Earth extends World {
     public void constructWorld() throws Exception {
         DefaultGroundCreator groundCreator = new DefaultGroundCreator();
         groundCreator.registerGround('.', Snow::new);
+        groundCreator.registerGround('/', Glass::new);
 
         List<String> map = Arrays.asList(
                 "........................................",
@@ -55,6 +56,16 @@ public class Earth extends World {
                 "........................................",
                 "........................................",
                 "........................................"
+        );
+
+        List<String> shopLayout = Arrays.asList(
+                "///////",
+                "/...../",
+                "/...../",
+                "/...../",
+                "/...../",
+                "/...../",
+                "///////"
         );
 
 
