447626f05921ad8992431bc4f712abfba1b0fa4d
Min Zhengyuan <zmin0008@student.monash.edu>
2025-11-02 18:21:57 +0800

    update interface for three stores

--- src/main/java/game/mysteriostore/MysterioStore.java
+++ src/main/java/game/mysteriostore/MysterioStore.java
@@ -1,6 +1,8 @@
 package game.mysteriostore;
 
+import game.actors.Seller;
 import game.mysteriostore.DimensionalStoreType;
+
 /**
  * Template for configuring a predefined Mysterio store.
  */
@@ -11,4 +13,10 @@ public interface MysterioStore {
      */
     DimensionalStoreType getType();
 
+    /**
+     * Creates the seller assigned to this store template.
+     *
+     * @return configured seller instance for the store
+     */
+    Seller createSeller();
 }
