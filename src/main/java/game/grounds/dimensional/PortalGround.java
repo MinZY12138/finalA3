1c4b8be445abcfa7ab173c37f0553910fabd219f
Min Zhengyuan <zmin0008@student.monash.edu>
2025-11-02 22:45:47 +0800

    motified portalground as a super class

--- src/main/java/game/grounds/dimensional/PortalGround.java
+++ src/main/java/game/grounds/dimensional/PortalGround.java
@@ -1,26 +1,46 @@
-package game.mysteriostore;
+package game.grounds.dimensional;
 
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
 import game.actions.EnterStoreAction;
-import game.grounds.GroundInfo;
+import game.mysteriostore.DimensionalStoreType;
+import game.mysteriostore.MysterioStoreSession;
 
-public class PortalGround extends Ground {
-    private final DimensionalStoreType type;
+import java.util.Objects;
+/**
+ * Base ground that exposes a portal into an active dimensional store.
+ */
+public class PortalGround extends Ground implements DimensionalSite {
 
-    public PortalGround(DimensionalStoreType type) {
+    private final DimensionalStoreType storeType;
+    private MysterioStoreSession activeSession;
 
-        super(GroundInfo.PORTAL.getDISPLAY_CHAR(), GroundInfo.PORTAL.getNAME());
-        this.type = type;
+    public PortalGround(char displayChar, String name, DimensionalStoreType storeType) {
+        super(displayChar, name);
+        this.storeType = Objects.requireNonNull(storeType);
     }
 
     @Override
-    public boolean canActorEnter(Actor actor) { return true; }
+    public ActionList allowableActions(Actor actor, Location location, String direction) {
+        ActionList actions = super.allowableActions(actor, location, direction);
+        if (activeSession != null && activeSession.isOpen()) {
+            actions.add(new EnterStoreAction(activeSession));
+        }
+        return actions;
+    }
 
     @Override
-    public ActionList allowableActions(Actor actor, Location location, String direction) {
-        return java.util.List.of(new EnterStoreAction(type, location));
+    public DimensionalStoreType getStoreType() {
+        return storeType;
+    }
+
+    public void activateStore(MysterioStoreSession session) {
+        this.activeSession = Objects.requireNonNull(session);
+    }
+
+    public void deactivateStore() {
+        this.activeSession = null;
     }
 }
