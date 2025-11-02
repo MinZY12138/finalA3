efb6d3d9262a2ac9b6f83d5ff67ecb6fccd52b2a
Min Zhengyuan <zmin0008@student.monash.edu>
2025-11-02 16:58:06 +0800

    Create 0 as portal of store.

---
+++ src/main/java/game/grounds/dimensional/PortalGround.java
@@ -0,0 +1,25 @@
+package game.mysteriostore;
+
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actions.EnterStoreAction;
+import game.grounds.dimensional.DimensionalStoreType;
+
+public class PortalGround extends Ground {
+    private final DimensionalStoreType type;
+
+    public PortalGround(DimensionalStoreType type) {
+        super('O');
+        this.type = type;
+    }
+
+    @Override
+    public boolean canActorEnter(Actor actor) { return true; }
+
+    @Override
+    public ActionList allowableActions(Actor actor, Location location, String direction) {
+        return java.util.List.of(new EnterStoreAction(type, location));
+    }
+}
