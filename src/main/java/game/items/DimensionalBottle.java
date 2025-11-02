069288415d4e4a64a31295f39f90bac55e1da70c
cheehsian-1120 <ctay0040@student.monash.edu>
2025-11-03 05:31:09 +0800

    Add Javadoc - Add meaningful Javadoc to the DimensionalBottle and BreakBottleAction class

--- src/main/java/game/items/DimensionalBottle.java
+++ src/main/java/game/items/DimensionalBottle.java
@@ -5,16 +5,40 @@ import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.Location;
 import game.actions.BreakBottleAction;
 
+/**
+ * <h1>DimensionalBottle Class</h1>
+ * <p>
+ *     The {@code DimensionalBottle} is an {@link Item} that actors can throw it on the ground.
+ *     The ground will turn to the {@link DimensionalGround} and then produce a door leading to
+ *     a mystery store after three turns.
+ * </p>
+ *
+ * @author Tay Chee Hsian
+ * @version 1.0.0
+ * @since 2025-11-03
+ */
 public class DimensionalBottle extends Item {
 
+    /**
+     * The ground that the store's door will appear.
+     */
     private DimensionalGround dimensionalGround;
 
+    /**
+     * Constructor of the DimensionalGround.
+     */
     public DimensionalBottle() {
         super(ItemInfo.DIMENSIONAL_BOTTLE.getNAME(),
                 ItemInfo.DIMENSIONAL_BOTTLE.getCHAR(),
                 ItemInfo.DIMENSIONAL_BOTTLE.isPORTABLE());
     }
 
+    /**
+     * This method allows actor can throw the dimensional bottle on the ground.
+     *
+     * @param location the location of the ground on which the item lies
+     * @return a break bottle action
+     */
     @Override
     public ActionList allowableActions(Location location) {
         ActionList actions = super.allowableActions(location);
@@ -22,6 +46,11 @@ public class DimensionalBottle extends Item {
         return actions;
     }
 
+    /**
+     * Set up the dimensional ground.
+     *
+     * @param dimensionalGround the ground that will appear store's door
+     */
     public void setDimensionalGround(DimensionalGround dimensionalGround) {
         this.dimensionalGround = dimensionalGround;
     }
