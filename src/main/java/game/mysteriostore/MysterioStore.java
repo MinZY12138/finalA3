e4e287e1747f1a8bb8b5f2fe9ab970f9125a475f
Min Zhengyuan <zmin0008@student.monash.edu>
2025-11-02 18:07:04 +0800

    Created a interface for three stores

---
+++ src/main/java/game/mysteriostore/MysterioStore.java
@@ -0,0 +1,14 @@
+package game.mysteriostore;
+
+import game.mysteriostore.DimensionalStoreType;
+/**
+ * Template for configuring a predefined Mysterio store.
+ */
+public interface MysterioStore {
+
+    /**
+     * @return the type of store represented by this template
+     */
+    DimensionalStoreType getType();
+
+}
